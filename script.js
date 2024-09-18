b377fa93dad8fcfd76608223f8b99b2f169c8601
Ralphie Guappone <ralphie.guappone@gmail.com>
2024-09-18 10:54:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,19 @@
  * Then, using your Literals, console.log "my favorite song is..." with the song's value
  */
 
+let myFavoriteSong = "Ghost";
+console.log(`my favorite song is ${myFavoriteSong}`);
+
 /*
  * Prompt 2:
  *
  * Create 3 variables using let and const, then console.log each of them, using a string literal (` ${} `} in the log
  */
+
+let myName = 'Ralphie';
+let petName = 'Ocean';
+const favoritePlace = 'Oak Island';
+console.log(`My name is ${myName} and my dogs name is ${petName} and my favorite place in the world is ${favoritePlace}`);
 
 /*
  * Prompt 3:
@@ -22,6 +30,15 @@
  *   - calculate the remainder of dividing your variable by 2
  */
 
+let num = 40;
+console.log(`My number is ${num}`);
+console.log(`Addition: ${num + 5}`);
+console.log(`Subtraction: ${num - 7}`);
+console.log(`Multiply by 3: ${num * 3}`);
+console.log(`Divided by 7: ${num / 7}`);
+console.log(`Remainder by 2: ${num % 2}`);
+
+
 /*
  * Prompt 4:
  *
@@ -33,13 +50,24 @@
  * and lastName variables.
  * Run a console.log that says 'hello my name is..." with your fullName. 
  */
-
+let firstName = 'Ralphie';
+let lastName = 'Guappone';
+let fullName = firstName + ' ' + lastName
+console.log(`hello my name is ${fullName}`)
 
 /* Arrays */
 
 // Create an array of Movies, then log the 2nd movie in your array
 // Use your array methods to add a movie in to the Front of the array, then the Back of the Array, then to the 2nd index point of the array
 
+let movies = ['Wrong Turn', 'Tooth Fairy', 'Harold and Kumar', 'Brokeback Mountain'];
+console.log(movies[1]);
+movies.unshift('Hills Have Eyes');
+console.log(movies);
+movies.push('Scary Movie 3');
+console.log(movies);
+movies.splice(1, 0, 'Spongebob Squarepants');
+console.log(movies);
 
 /*
  *
@@ -47,22 +75,27 @@
  */
 
 let array1 = ['Marty Marion', 'Marty Feldman', 'Marty McFly', 'Marty Marion'];
+console.log(`array1: ${array1[2]}`);
+
 let array2 = [
   ['Marty Feldman', 'Marty Marion'],
   ['Marty Stuart', 'Marty McFly'],
   ['Marty Jannetty', 'Marty Robbins']
 ];
+console.log(`array2: ${array2[1][1]}`);
+
 let array3 = [
   ['Marty Feldman', ['Marty Marion']],
   ['Marty Stuart', ['Marty Janetty', ['Marty McFly'], 'Marty Robbins']]
 ];
-
+console.log(`array3: ${array3[1][1][1]}`);
 /*
  *
  * Use the length of the array below to retrieve the second to last item.
  */
 
 let array4 = ['a', 'b', 'c', 'd', 'e'];
+console.log(array4[array4.length-2]);
 
 /*
  *
@@ -76,10 +109,20 @@ let cathleen = ['Cathleen', 186, 'New York'];
 
 // Cathleen decides that Thom can't be named "Thom" anymore. Remove "Thom" from
 // the thom array and replace it with "Tom".
+thom.shift();
+thom.unshift('Tom');
+console.log(thom);
 
 // Karolin just had her birthday; change Karolin's array to reflect her being
 // a year older.
+karolin[1] = 17;
+console.log(karolin);
 
 // Change Cathleen's hometown from New York to "Gotham City".
+cathleen[2] = 'Gotham City';
+console.log(cathleen);
 
 // Remove "Pittsburgh" from Kristyn's array and add "Oakland".
+kristyn.pop();
+kristyn.push('Oakland');
+console.log(kristyn);
